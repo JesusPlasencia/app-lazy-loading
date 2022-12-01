1ff9a68dd962b403687055a2b58c0d48e28ded96
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2022-12-01 17:05:10 -0500

    Finish course

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {registerImage} from './lazy';
 
-console.log('Happy hacking :)')
+//! API URL
+const API_FOX = 'https://randomfox.ca/floof/';
+
+//! NODOS
+const IMAGES_CONTAINER = document.getElementById('images');
+const BTN_ADD_IMAGE = document.getElementById('btnAddImage');
+
+//! Funcion Fetch
+const addImage = async () => {
+    const data = await fetch(API_FOX);
+    const json = await data.json();
+    
+    const image = document.createElement('img');
+    image.classList = 'mx-auto';
+    image.dataset.src = json.image;
+    image.width = "320";
+    image.alt = "";
+
+    const imageContainer = document.createElement('div');
+    imageContainer.classList = 'p-4 mx-auto bg-gray-300';
+    imageContainer.style.maxWidth = "320px";
+    imageContainer.style.maxWidth = "320px";
+
+    imageContainer.appendChild(image);
+    IMAGES_CONTAINER.appendChild(imageContainer);
+    registerImage(imageContainer);
+}
+
+//! Evento Click al Btn
+BTN_ADD_IMAGE.addEventListener('click', addImage);
